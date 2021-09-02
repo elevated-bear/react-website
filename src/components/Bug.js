3d2cd139f88886f46022268f1c007749239d8bd5
lane <40241524+lanetrobee@users.noreply.github.com>
2021-09-01 23:22:59 -0500

    added some nice boxes to listed bugs

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function Bug({ bug }) {
+
+    const bugBubble = {
+        border: "2px solid green",
+        'border-radius': "0.5rem",
+        padding: "0.5rem",
+        margin: "0.5rem"
+    }
+
     return (
-        <div>
+        <div style={bugBubble}>
             <div>ID: {bug.id}</div>
             <div>Bug name: {bug.name}</div>
             <div>Bug description: {bug.desc}</div>
