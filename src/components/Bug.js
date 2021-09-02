608420fe18b1a65c5bb7e931311e255b8c59e3db
lane <40241524+lanetrobee@users.noreply.github.com>
2021-09-01 23:12:01 -0500

    minor fix to hierarchy of elements

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Bug({ bug }) {
     return (
-        <p>
+        <div>
             <div>ID: {bug.id}</div>
             <div>Bug name: {bug.name}</div>
             <div>Bug description: {bug.desc}</div>
-        </p>
+        </div>
 
     );
 }
